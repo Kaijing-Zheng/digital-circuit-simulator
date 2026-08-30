61bb2b3ba55ea02b937e32075c12be06e2240ec8
Kaijing Zheng <kaijing.zheng@duke.edu>
2026-08-29 22:15:10 -0400

    Convert simulator to Spring Boot backend

---
+++ src/main/java/com/circuitsim/HealthController.java
@@ -0,0 +1,13 @@
+package com.circuitsim;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/health")
+    public String health() {
+        return "Circuit simulator backend is running";
+    }
+}
