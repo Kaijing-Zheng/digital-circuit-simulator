be4c8fa39515f284d88b8a00ba8bbcdc1fd02d90
Kaijing Zheng <kaijing.zheng@duke.edu>
2026-08-29 21:25:53 -0400

    Add base circuit component abstraction

---
+++ src/main/java/com/circuitsim/Component.java
@@ -0,0 +1,16 @@
+package com.circuitsim;
+
+public abstract class Component {
+
+    private String name;
+
+    public Component(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract boolean evaluate();
+}
